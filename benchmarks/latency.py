bea6acc0b31f251342189f8ebd9208fbead3bc35
Keisuke Kamahori <kamahori@cs.washington.edu>
2024-02-06 07:54:00 +0000

    Add latency evaluation

--- benchmarks/latency.py
+++ benchmarks/latency.py
@@ -27,18 +27,6 @@ if __name__ == "__main__":
         '--cpu-offload', type=int, default=1, choices=[0, 1],
         help='0: exeute at GPU (baseline), 1: offload to CPU.'
     )
-    parser.add_argument(
-        '--input', type=str, default='Mistral AI is a',
-        help='Input text to generate.'
-    )
-    parser.add_argument(
-        '--input-token', type=int, default=20,
-        help='Number of tokens to generate.',
-    )
-    parser.add_argument(
-        '--output-token', type=int, default=20,
-        help='Number of tokens to generate.',
-    )
 
     args = parser.parse_args() 
 
@@ -60,25 +48,26 @@ if __name__ == "__main__":
     model = FiddlerMixtral(args)
     n_sample = 100
 
-    for input_token, output_token in zip([16, 32, 64, 128], [16, 32, 64, 128, 256, 512]):
-        idx_text = 0
-        prefill_time_sum, decode_time_sum, hit_rate_sum = 0, 0, 0
-        for _ in range(n_sample):
-            text = texts[idx_text]
-            idx_text += 1
-            if len(text.split()) < input_token:
-                # not enough input length
-                continue
-            prefill_time, decode_time, hit_rate = model.generate(
-                text, 
-                output_token=output_token, 
-                input_token=input_token
-            )
-            prefill_time_sum += prefill_time
-            decode_time_sum += decode_time
-            hit_rate_sum += hit_rate
-        print(
-            f'input_token: {input_token}, output_token: {output_token}, '
-            f'prefill_time: {prefill_time_sum / n_sample}, '
-            f'decode_time: {decode_time_sum / n_sample}, '
-            f'hit_rate: {hit_rate_sum / n_sample}')
+    for input_token in [16, 32, 64, 128]:
+        for output_token in [16, 32, 64, 128, 256, 512]:
+            idx_text = 0
+            prefill_time_sum, decode_time_sum, hit_rate_sum = 0, 0, 0
+            for _ in range(n_sample):
+                text = texts[idx_text]
+                idx_text += 1
+                if len(text.split()) < input_token:
+                    # not enough input length
+                    continue
+                prefill_time, decode_time, hit_rate = model.generate(
+                    text, 
+                    output_token=output_token, 
+                    input_token=input_token
+                )
+                prefill_time_sum += prefill_time
+                decode_time_sum += decode_time
+                hit_rate_sum += hit_rate
+            print(
+                f'input_token: {input_token}, output_token: {output_token}, '
+                f'prefill_time: {prefill_time_sum / n_sample}, '
+                f'decode_time: {decode_time_sum / n_sample}, '
+                f'hit_rate: {hit_rate_sum / n_sample}')
